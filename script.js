b8deffa2e67c7647a0055b97a1f8d000f99d1c43
efesezer48 <31394768+efesezer48@users.noreply.github.com>
2024-03-01 19:46:54 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    setTimeout(() => {
+        document.getElementById('landingPage').style.opacity = '1';
+    }, 200); // This timeout matches the CSS animation duration
+});
